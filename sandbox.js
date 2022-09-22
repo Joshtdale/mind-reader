fe5ea384d859c24fa31c21b8f21a7d549f14c36b
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-09-22 15:27:54 +0000

    update

--- sandbox.js
+++ sandbox.js
@@ -1,27 +1,73 @@
 const headText = document.getElementById('headText')
 const example = document.getElementById('example')
 const nextBtn = document.getElementById('next')
+const resetBtn = document.getElementById('resetBtn')
 var i = 0;
 
 
 const pages = [
     {
     headText: 'I can read your mind',
-    exampleText: 'Example text'
+
+    exampleText: '',
+    resetBtn: 'GO'
 
 },
 {
     headText: 'Pick a number from 01-99',
-    exampleText: 'New exampleText'
+    nextBtn: 'Next',
+    exampleText: 'when you have your number\nclick next',
+    resetBtn: ''
+},
+{
+    headText: 'Add both digits together to get a new number',
+    nextBtn: 'Next',
+    exampleText: 'Ex: 14 is 1 + 4 = 5\nClick next to proceed',
+    resetBtn: ''
+},
+{
+    headText: 'Subtract your new number from the original number',
+    nextBtn: 'Next',
+    exampleText: 'Ex: 14 - 5 = 9',
+    resetBtn: ''
 }
 ]
-function update(){
-headText.innerText = pages[i].headText
-example.innerText = pages[i].exampleText
+
+function increment(){
+    i++;
+    console.log(i)
+    update()
 }
-nextBtn.addEventListener('click', () => {
-i++;
-// console.log(i)
-update()
+
+function update(){
+    
+if (i === 0){
+    nextBtn.style.display = 'none';
+    console.log('i = 0')
+} else {
+    nextBtn.style.display = 'block';
+}
+headText.innerText = pages[i].headText
+nextBtn.innerText = pages[i].nextBtn
+example.innerText = pages[i].exampleText
+resetBtn.innerText = pages[i].resetBtn
+
+
+}
+
+resetBtn.addEventListener('click', () => {
+    if (i === 0){
+        increment()
+    }
 })
+
+nextBtn.addEventListener('click', () => {
+    if (i > 0){
+increment()
+    }
+})
+
+
+
 update()
+// nextBtn.style.display = 'none';
